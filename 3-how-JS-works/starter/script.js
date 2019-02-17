0aea5e32b1b86903805218e89912447fb15ceb85
KN <knny.nguyen@gmail.com>
2019-02-17 17:34:19 -0500

    lecture: hoisting

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,25 +1,34 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+calcAge(1990);
 
+function calcAge(year) {
+  console.log(2016 - year);
+}
+// calcAge function declaration is already available bc of hoisting
 
+retirement(1990);
+var retirement = function(year) {
+  console.log(65 - (2016 - year));
+};
+// however retirement will not work bc it is a function expression
 
+// variables
+console.log(age);
+var age = 23; // global execution context
+// in creation phase of variable object, code is scanned for variable declarations and variables are set to undefined
 
+function foo() {
+  var age = 65;
+  // different execution context
+  console.log(age);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+foo();
+console.log(age);
 ///////////////////////////////////////
-// Lecture: Scoping
 
+// Lecture: Scoping
 
 // First scoping example
 
@@ -37,8 +46,6 @@ function first() {
     }
 }
 */
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -62,16 +69,5 @@ function third() {
 }
 */
 
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
-
-
-
-
-
-
-
-
-
