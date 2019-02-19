3be41a5037fe27589ee6f822e48ad40b24063482
KN <knny.nguyen@gmail.com>
2019-02-18 23:10:03 -0500

    lecture: this keyword

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,31 +1,31 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
-calcAge(1990);
+// calcAge(1990);
 
-function calcAge(year) {
-  console.log(2016 - year);
-}
-// calcAge function declaration is already available bc of hoisting
+// function calcAge(year) {
+//   console.log(2016 - year);
+// }
+// // calcAge function declaration is already available bc of hoisting
 
-retirement(1990);
-var retirement = function(year) {
-  console.log(65 - (2016 - year));
-};
-// however retirement will not work bc it is a function expression
+// retirement(1990);
+// var retirement = function(year) {
+//   console.log(65 - (2016 - year));
+// };
+// // however retirement will not work bc it is a function expression
 
-// variables
-console.log(age);
-var age = 23; // global execution context
-// in creation phase of variable object, code is scanned for variable declarations and variables are set to undefined
+// // variables
+// console.log(age);
+// var age = 23; // global execution context
+// // in creation phase of variable object, code is scanned for variable declarations and variables are set to undefined
 
-function foo() {
-  var age = 65;
-  // different execution context
-  console.log(age);
-}
+// function foo() {
+//   var age = 65;
+//   // different execution context
+//   console.log(age);
+// }
 
-foo();
-console.log(age);
+// foo();
+// console.log(age);
 ///////////////////////////////////////
 
 // Lecture: Scoping
@@ -71,3 +71,26 @@ function third() {
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+
+console.log(this); // points to window object
+
+function calcAge(year) {
+  console.log(2016 - year);
+  console.log(this); // points to window object bc function is attached to global object
+}
+
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  calcAge: function() {
+    console.log(this); // points to john object
+    console.log(2016 - this.yearOfBirth);
+
+    function innerFunction() {
+      console.log(this); // points to window object bc it is a regular function not a method of an object
+    }
+    innerFunction();
+  }
+};
+
+john.calcAge();
