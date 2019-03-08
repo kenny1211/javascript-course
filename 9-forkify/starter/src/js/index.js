7ad9fd78c9edf5f11fdbe17b67a4e0239a867174
KN <knny.nguyen@gmail.com>
2019-03-07 19:06:21 -0500

    Final Project: API Test

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,19 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! Variable x is ${x}`);
+// https://www.food2fork.com/api/search
+// 26b58ff329206e7d1c47ae6121fada7c
+
+import axios from 'axios';
+
+async function getRecipes(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = '26b58ff329206e7d1c47ae6121fada7c';
+  try {
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// getRecipes('pizza');
